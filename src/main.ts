c29adce2a835a67f0fd026cc4c7ac816378d5be3
Jorge Herrera <jorgeluishm88@gmail.com>
2026-06-07 12:43:20 -0300

    feat: integra fluxo no main com injecao de dependencias e menu

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 import { PokeApiService } from "./services/PokeApiService";
+import { BoxService } from "./services/BoxService";
+import { CatalogoPokemon, PokemonResumo } from "./models/Pokemon";
+import { TerminalController } from "./controllers/TerminalController";
+import { LocalBoxError } from "./models/CustomErrors";
+import { erro } from "./utils/textFormatters";
 
 async function main() {
     const api = new PokeApiService();
+    const box = new BoxService();
 
-    const pikachu = await api.buscar("pikachu");
-
-    if (pikachu) {
-        console.log(`[OK] Pokémon encontrado: ${pikachu.nome}`);
-        console.log(
-            `#${pikachu.id} - ${pikachu.nome} | Tipos: ${pikachu.tipos.join(", ")} | Altura: ${pikachu.altura} | Peso: ${pikachu.peso}`
-        );
-    } else {
-        console.log("[ERRO] Pokémon não encontrado.");
+    let dadosIniciais: PokemonResumo[] = [];
+    try {
+        dadosIniciais = await box.carregar();
+    } catch (e) {
+        if (e instanceof LocalBoxError) {
+            console.log(erro(e.message));
+        }
     }
+
+    const catalogo = new CatalogoPokemon(dadosIniciais);
+    const controller = new TerminalController(api, catalogo, box);
+
+    await controller.iniciar();
 }
 
 main();
